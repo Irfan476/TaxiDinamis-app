78e51ae78007a577460c457939e2fa49fa68927d
Irfan476 <irfanandryanto47@students.amikom.ac.id>
2022-03-22 18:56:42 +0700

    App Taxi dengan class dan object

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.Name = "Andre";
+            taxi.Duty = true;
+            taxi.Numberpass = 8;
+
+            taxi.TaxiInfo();
+            taxi.PickupPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    public class Taxi
+    {
+        public string Name { get; set; }
+        public bool Duty { get; set; }
+        public int Numberpass { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver name : {0}", Name);
+            if (Duty == true)
+            {
+                Console.WriteLine("On duty : yes");
+            }
+            else
+            {
+                Console.WriteLine("On duty : no");
+            }
+            Console.WriteLine("Number of passenger : {0}", Numberpass);
+        }
+
+        public void PickupPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", Name);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} sedang mengantar penumpang", Name);
+        }
+    }
+}
